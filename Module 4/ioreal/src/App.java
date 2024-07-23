a8643e70cd26d9ed35e343d2f91b0d7db222a298
WhenIBOnThaMic <hoangminhtom10@gmail.com>
2024-07-23 21:15:47 +0700

    setup for module 4 + future module

--- Module 4/ioreal/src/App.java
+++ Module 4/ioreal/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        StdDraw.setXscale(0, 1);
+        StdDraw.setYscale(0, 1);
+        StdDraw.setPenRadius(0.01);
+        StdDraw.setPenColor();
+        StdDraw.line(0,0,1,0);
+        StdIn.readDouble();
     }
 }
