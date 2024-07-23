8a6c075a109a0447bd84753366a0c87f70b92faa
WhenIBOnThaMic <hoangminhtom10@gmail.com>
2024-07-23 21:16:06 +0700

    outlined idea + loop

---
+++ Module 4/ioreal/src/Checkerboard.java
@@ -0,0 +1,17 @@
+public class Checkerboard {
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(args[0]);
+        StdDraw.setScale(0, n);
+        boolean blue = true;
+        if (n % 2 == 0) blue = false;
+        //setting up stuff
+        for (int row = 0; row < n; row++) {
+            for (int column = 0; column < n; column++) {
+                if (blue) {
+                    
+                }
+            }
+        }
+    }
+}
