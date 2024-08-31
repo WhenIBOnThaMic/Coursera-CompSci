7f45ece26d68b7c30a96352b3654810a8e2d3669
WhenIBOnThaMic <hoangminhtom10@gmail.com>
2024-08-31 16:10:34 +0700

    toString() method finished
    finished the toString() method for ColorHSB, which returns the color as a string of hue, saturation, and brightness

--- Module 9/oop2/src/ColorHSB.java
+++ Module 9/oop2/src/ColorHSB.java
@@ -1,20 +1,38 @@
 public class ColorHSB {
-private final h = 
+    private final int hue;
+    private final int saturation;
+    private final int brightness;
+
     // Creates a color with hue h, saturation s, and brightness b.
-    public ColorHSB(int h, int s, int b) {
+    public ColorHSB(int h, int s, int b) throws IllegalArgumentException {
+        if (h > 359 || h < 0) throw new IllegalArgumentException();
+        if (s < 0 || s > 100) throw new IllegalArgumentException();
+        if (s < 0 || s > 100) throw new IllegalArgumentException();
+        //conditions 
+        hue = h;
+        saturation = s;
+        brightness = b;        
 
     }
-
+    
     // Returns a string representation of this color, using the format (h, s, b).
-    public String toString()
-
+    public String toString() {
+        String stringRep = "(" + hue + ", " + saturation + ", " + brightness + ")";
+        return stringRep; 
+    }
+    
     // Is this color a shade of gray?
-    public boolean isGrayscale()
+    public boolean isGrayscale() {
 
+    }
+    /*
     // Returns the squared distance between the two colors.
-    public int distanceSquaredTo(ColorHSB that)
-
+    public int distanceSquaredTo(ColorHSB that) {
+    }
+    */
     // Sample client (see below).
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        ColorHSB a = new ColorHSB(0, 0, 50);
+    }
 
 }
