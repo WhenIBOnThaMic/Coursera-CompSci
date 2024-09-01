e012e02978910537d70dac856f69eb5683a52a55
WhenIBOnThaMic <hoangminhtom10@gmail.com>
2024-09-01 15:08:32 +0700

    instance variables + constructors finished Clock.java
    instance variables + constructors finished for Clock data type in Clock.java

--- Module 9/oop2/src/App.java
+++ Module 9/oop2/src/App.java
@@ -1,7 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        In input = new In(args[0]);
-        String[] a = input.readAllStrings();
-        System.out.println(a.length/4);
+        String a = "2";
+
     }
 }
--- Module 9/oop2/src/Clock.java
+++ Module 9/oop2/src/Clock.java
@@ -1,23 +1,48 @@
 public class Clock {
-
+    private int hour;
+    private int minutes;
     // Creates a clock whose initial time is h hours and m minutes.
-    public Clock(int h, int m)
+    public Clock(int h, int m) throws IllegalArgumentException {
+        if (h < 0 || h > 23 || m < 0 || m > 59) throw new IllegalArgumentException();
+        hour = h;
+        minutes = m;
+    }
 
     // Creates a clock whose initial time is specified as a string, using the format HH:MM.
-    public Clock(String s)
+    public Clock(String s) throws IllegalArgumentException {
+        if (s.length() > 5) throw new IllegalArgumentException();
+        if ( Integer.parseInt(s.substring(0, 1)) < 0 || Integer.parseInt(s.substring(0, 1)) > 23 ) throw new IllegalArgumentException();
+        if ( Integer.parseInt(s.substring(3, 4)) < 0 || Integer.parseInt(s.substring(0, 1)) > 59 ) throw new IllegalArgumentException();
+        if (s.charAt(2) != ':') throw new IllegalArgumentException();
+        hour = Integer.parseInt(s.substring(0,1));
+        minutes = Integer.parseInt(s.substring(3, 4));
 
+    }
+
+    /*
     // Returns a string representation of this clock, using the format HH:MM.
-    public String toString()
+    public String toString(){
+
+    }
 
     // Is the time on this clock earlier than the time on that one?
-    public boolean isEarlierThan(Clock that)
+    public boolean isEarlierThan(Clock that) {
+
+    }
 
     // Adds 1 minute to the time on this clock.
-    public void tic()
+    public void tic() {
+
+    }
 
     // Adds Δ minutes to the time on this clock.
-    public void toc(int delta)
+    public void toc(int delta) {
 
+    }
+    */
     // Test client (see below).
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        String a = "23:59";
+        Clock b = new Clock(a);
+    }
 }
