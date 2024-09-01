655f5b5252be84a7e67b329ad9a8edf6b4d4c19f
WhenIBOnThaMic <hoangminhtom10@gmail.com>
2024-09-01 15:22:16 +0700

    minor fixes to instance variable's name

--- Module 9/oop2/src/Clock.java
+++ Module 9/oop2/src/Clock.java
@@ -1,11 +1,11 @@
 public class Clock {
     private int hour;
-    private int minutes;
+    private int minute;
     // Creates a clock whose initial time is h hours and m minutes.
     public Clock(int h, int m) throws IllegalArgumentException {
         if (h < 0 || h > 23 || m < 0 || m > 59) throw new IllegalArgumentException();
         hour = h;
-        minutes = m;
+        minute = m;
     }
 
     // Creates a clock whose initial time is specified as a string, using the format HH:MM.
@@ -14,17 +14,23 @@ public class Clock {
         if ( Integer.parseInt(s.substring(0, 2)) < 0 || Integer.parseInt(s.substring(0, 2)) > 23 ) throw new IllegalArgumentException();
         if ( Integer.parseInt(s.substring(3, 5)) < 0 || Integer.parseInt(s.substring(3, 5)) > 59 ) throw new IllegalArgumentException();
         if (s.charAt(2) != ':') throw new IllegalArgumentException();
-        hour = Integer.parseInt(s.substring(0,1));
-        minutes = Integer.parseInt(s.substring(3, 4));
+        hour = Integer.parseInt(s.substring(0,2));
+        minutes = Integer.parseInt(s.substring(3, 5));
 
     }
 
-    /*
+    
     // Returns a string representation of this clock, using the format HH:MM.
     public String toString(){
+        String hour2;
+        String minute2;
+        if (hour < 10) {hour2 = "0" + hour;}
+        if (minute < 10) {minute2 = "0" + hour;}
 
+        String a = "";
+        return a;
     }
-
+    /*
     // Is the time on this clock earlier than the time on that one?
     public boolean isEarlierThan(Clock that) {
 
@@ -42,5 +48,11 @@ public class Clock {
     */
     // Test client (see below).
     public static void main(String[] args) {
+        String sus = "01:03";
+        Clock a = new Clock(1, 3);
+        Clock b = new Clock(sus);
+        System.out.println(a.toString());
+        System.out.println(b.toString());
+
     }
 }
