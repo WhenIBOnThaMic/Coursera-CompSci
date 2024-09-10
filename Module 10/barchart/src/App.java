ab29f1b0946bc49a61a007e73b08671992771d83
WhenIBOnThaMic <hoangminhtom10@gmail.com>
2024-09-10 20:52:26 +0700

    getting started on BarChartRacer
    did some tests with In data type to ensure smooth program run

--- Module 10/barchart/src/App.java
+++ Module 10/barchart/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        In a = new In();
+        String first = a.readLine();
+        String second = a.readLine();
+        String third = a.readLine();
+
+
+        String[] b = a.readAllLines();
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }
     }
 }
--- Module 10/barchart/src/BarChartRacer.java
+++ Module 10/barchart/src/BarChartRacer.java
@@ -1,3 +1,12 @@
 public class BarChartRacer {
+    public static void main(String[] args) {
+        In inputStream = new In();
+        String title = inputStream.readLine();
+        String xAxisLabel = inputStream.readLine();
+        String dataSource = inputStream.readLine();
+        inputStream.readLine(); //skip blank line
+        //labels done
 
+        
+    }
 }
